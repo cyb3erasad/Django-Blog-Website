45fb7364fa552814c2bd6c1a1f32bc8282812c4f
cyb3erasad <cyb3rasad@gmail.com>
2026-03-14 00:10:16 +0500

    add signup.html in template and fix erros in postlistvew and base file

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,6 @@ from .views import PostListView, PostDetailView, SignupView
 
 urlpatterns = [
     path('', PostListView.as_view(), name='post_list'),
-    path('<slug:slug>/', PostDetailView.as_view(), name='post_detail'),
     path('signup/', SignupView.as_view(), name='signup'),
+    path('<slug:slug>/', PostDetailView.as_view(), name='post_detail'),
 ]
